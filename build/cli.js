1d4c883b7db3045e9cf32f4415d0bddb98367455
SenudaJayalath <senudajayalath1@gmail.com>
2021-12-20 18:31:57 +0530

    Add error checking to cli.ts

--- build/cli.js
+++ build/cli.js
@@ -24,38 +24,39 @@ dotenv_1.default.config();
 var web3 = new web3_1.default(new web3_1.default.providers.HttpProvider('http://localhost:7545'));
 const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 const STORE_ADDRESS = process.env.STORE_ADDRESS;
+const fs_1 = require("fs");
 function main(argv) {
-    function sendIPFS() {
+    function sendIPFS(id, fileName) {
         return __awaiter(this, void 0, void 0, function* () {
-            const buffer = Buffer.from([8, 6, 4, 7, 3]);
-            //const file = readFileSync('./build/filename.txt', 'utf-8');
-            //let buffer= Buffer.from(file)
+            let accounts = yield web3.eth.getAccounts();
+            if (isNaN(id)) {
+                console.log("err");
+                return;
+            }
+            let account = accounts[id];
+            if (!(0, fs_1.existsSync)(fileName)) {
+                console.log("File not found");
+                return;
+            }
+            const file = (0, fs_1.readFileSync)(fileName, 'utf-8');
+            let buffer = Buffer.from(file);
             yield ipfs.add(buffer, (err, ipfsHash) => __awaiter(this, void 0, void 0, function* () {
                 if (!err) {
-                    console.log(ipfsHash[0].hash);
-                    let accounts = yield web3.eth.getAccounts();
                     const contract = new web3.eth.Contract(Store_json_1.default.abi, STORE_ADDRESS);
-                    const receipt = yield contract.methods.storeCID("sasdad").send({ from: accounts[0] });
-                    console.log(receipt.events.itemSaved.returnValues);
+                    const receipt = yield contract.methods.storeCID(ipfsHash[0].hash).send({ from: account, gas: 6721975, gasPrice: '30000000' });
+                    console.log(receipt.events.itemSaved.returnValues.id);
                 }
                 else {
                     console.log(err);
                 }
             }));
-            //const contractJson = JSON.stringify(ABI.abi);
-            //web3.eth.getAccounts().then(console.log)
-            //     const contract = new web3.eth.Contract(ABI.abi,STORE_ADDRESS);
-            //     const id = await contract.methods.newItemId().call()
-            //     console.log(id)
-            // //     const contract = new web3.eth.Contract(ABI.abi,STORE_ADDRESS);
-            // const receipt = await contract.methods.storeCID("sasdad").send({from:'0x924Af19A16Ca4dEeb2329F90fdbe8871cDD31508'})
-            // //     // console.log(await receipt.call({from:'0x924Af19A16Ca4dEeb2329F90fdbe8871cDD31508'}))
-            //     console.log(receipt.events.itemSaved.returnValues)
-            //     // web3.eth.getAccounts().then(console.log)
         });
     }
-    if (argv[0] == "--test") {
-        sendIPFS();
+    if (argv[0] == "--account" && argv[2] == "--filePath") {
+        sendIPFS(+argv[1], argv[3]);
+    }
+    else {
+        console.log("Invalid Input: Enter Account Number");
     }
 }
 main(process.argv.slice(2));
--- src/cli.ts
+++ src/cli.ts
@@ -12,36 +12,39 @@ var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
 
 const ipfs =new IPFS({host:'ipfs.infura.io',port:5001,protocol:'https'})
 const STORE_ADDRESS=process.env.STORE_ADDRESS
-import { readFileSync } from 'fs';
+import { readFileSync,existsSync } from 'fs';
+
+
+type accountRange = 0|1|2|3|4|5|6|7|8|9
 
 function main(argv:any){
-    async function sendIPFS(){
-        const buffer =Buffer.from([8,6,4,7,3]);
-        //const file = readFileSync('./build/filename.txt', 'utf-8');
-        //let buffer= Buffer.from(file)
+    async function sendIPFS(id:number,fileName:string){
+        let accounts = await web3.eth.getAccounts()
+        if (isNaN(id)){
+            console.log("err")
+            return
+        }
+        let account=accounts[id]
+        if(!existsSync(fileName)) {
+            console.log("File not found");
+            return
+          }
+        const file = readFileSync(fileName, 'utf-8');    
+        let buffer= Buffer.from(file)
         await ipfs.add(buffer,async(err:any,ipfsHash:string)=>{
             if (!err){
-                let accounts = await web3.eth.getAccounts()
                 const contract = new web3.eth.Contract(ABI.abi,STORE_ADDRESS);
-                const receipt = await contract.methods.storeCID(ipfsHash[0].hash).send({from:accounts[0]})
-                console.log(receipt.events.itemSaved.returnValues)
+                const receipt = await contract.methods.storeCID(ipfsHash[0].hash).send({from:account, gas: 6721975, gasPrice: '30000000'})
+                console.log(receipt.events.itemSaved.returnValues.id)
             }else{
                 console.log(err)
             }
         })
-        //const contractJson = JSON.stringify(ABI.abi);
-        //web3.eth.getAccounts().then(console.log)
-    //     const contract = new web3.eth.Contract(ABI.abi,STORE_ADDRESS);
-    //     const id = await contract.methods.newItemId().call()
-    //     console.log(id)
-    // //     const contract = new web3.eth.Contract(ABI.abi,STORE_ADDRESS);
-    // const receipt = await contract.methods.storeCID("sasdad").send({from:'0x924Af19A16Ca4dEeb2329F90fdbe8871cDD31508'})
-    // //     // console.log(await receipt.call({from:'0x924Af19A16Ca4dEeb2329F90fdbe8871cDD31508'}))
-    //     console.log(receipt.events.itemSaved.returnValues)
-    //     // web3.eth.getAccounts().then(console.log)
     }
-    if (argv[0] == "--test"){
-        sendIPFS()
-     }
+    if (argv[0] == "--account" && argv[2] == "--filePath" ){
+        sendIPFS(+argv[1],argv[3])
+    }else {
+         console.log("Invalid Input: Enter Account Number")
+    }
 }
 main(process.argv.slice(2))
